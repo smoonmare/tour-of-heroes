c33604356495482cc823134952c0aa2425d27f84
smoonmare <smoonmare@gmail.com>
2021-02-18 20:21:15 -0800

    Add click event binding, handler. Refactored to show details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,11 +8,15 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  selectedHero?: Hero;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
   heroes = HEROES;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
