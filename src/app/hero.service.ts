6f1ed1c0ee3b506559a7d4a392833eeca3d44392
smoonmare <smoonmare@gmail.com>
2021-02-21 21:24:06 -0800

    Error handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { MessageService } from './message.service';
 import { Hero } from '../app/heroes/heroes.interface';
 import { HEROES } from '../app/mock-heroes';
@@ -9,20 +10,43 @@ import { HEROES } from '../app/mock-heroes';
   providedIn: 'root'
 })
 export class HeroService {
-  private heroesUrl = 'api/heroes'; // URL to  web api
+  private heroesUrl = 'api/heroes'; // URL to  web api.
   
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-   return this.http.get<Hero[]>(this.heroesUrl);
+   return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
+    // TODO: send the message _after_ fetching the hero.
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id)!);
+  }
+
+  /**
+   * Handle Http operator that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed.
+   * @param result - optional value to return as the observable result.
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure.
+      console.error(error); // Log to console instead.
+
+      // TODO: better job of transforming error for user consumption.
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app run by returning an empty result.
+      return of(result as T);
+    }
   }
 
   /* Log a HeroService message with the MessageService */
