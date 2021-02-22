b6ddcc9e6a6666003e02d1f4200069dfa7ce559c
smoonmare <smoonmare@gmail.com>
2021-02-21 21:47:48 -0800

    Update heroes: button, components and service.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,6 +11,9 @@ import { HEROES } from '../app/mock-heroes';
 })
 export class HeroService {
   private heroesUrl = 'api/heroes'; // URL to  web api.
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
   
   constructor(
     private http: HttpClient,
@@ -30,6 +33,14 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`Updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
